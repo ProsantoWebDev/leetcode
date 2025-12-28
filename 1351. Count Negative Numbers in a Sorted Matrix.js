4e1316efaa09cf0a34b945c9e28cb37b4f7ab93e
ProsantoWebDev <wpdevprosanto@gmail.com>
2025-12-28 11:33:21 +0600

    update

---
+++ 1351. Count Negative Numbers in a Sorted Matrix.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var countNegatives = function (grid) {
+  result = [];
+  arr = [].concat(...grid).sort();
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < 0) {
+      result.push(arr[i]);
+    }
+  }
+  if (result.length < 0) {
+    return 0;
+  } else {
+    return result.length;
+  }
+};
+
+grid = [
+  [4, 3, 2, -1],
+  [3, 2, 1, -1],
+  [1, 1, -1, -2],
+  [-1, -1, -2, -3],
+];
+grid2 = [
+  [3, 2],
+  [1, 0],
+];
+console.log(countNegatives(grid2));
