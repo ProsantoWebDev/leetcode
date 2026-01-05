9f06de0e00e6f821b4c77465892a271b9d2e8c7e
ProsantoWebDev <wpdevprosanto@gmail.com>
2026-01-05 13:04:53 +0600

    'update'

--- 21. Merge Two Sorted Lists.js
+++ 21. Merge Two Sorted Lists.js
@@ -10,19 +10,32 @@
  * @param {ListNode} list2
  * @return {ListNode}
  */
-const mergeTwoLists = function (l1, l2) {
-  // Base cases
-  if (l1 === null) return l2;
-  if (l2 === null) return l1;
-
-  // Recursive case
-  if (l1.val < l2.val) {
-    l1.next = mergeTwoLists(l1.next, l2);
-    return l1;
-  } else {
-    l2.next = mergeTwoLists(l1, l2.next);
-    return l2;
+class ListNode {
+  constructor(val = 0, next = null) {
+    this.val = val;
+    this.next = next;
   }
+}
+
+var mergeTwoLists = function (list1, list2) {
+  let dummy = new ListNode();
+  let currNode = dummy;
+  while (list1 != null && list2 !== null) {
+    if (list1.val < list2.val) {
+      currNode.next = list1;
+      list1 = list1.next;
+    } else {
+      currNode.next = list2;
+      list2 = list2.next;
+    }
+    currNode = currNode.next;
+  }
+  if (list1 != null) {
+    currNode.next = list1;
+  } else {
+    currNode.next = list2;
+  }
+  return dummy.next;
 };
 (list1 = [1, 2, 4]), (list2 = [1, 3, 4]);
 console.log(mergeTwoLists(list1, list2));
