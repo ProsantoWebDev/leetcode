d7d1e0c1798ef0457a67f43b84af4bb20327ec31
ProsantoWebDev <wpdevprosanto@gmail.com>
2026-01-16 03:13:20 +0600

    update

--- index.js
+++ index.js
@@ -7,3 +7,11 @@ for (let i = 0; i < squares.length; i++) {
         console.log(l)
         console.log(`total:${total}`)
     }
+
+
+//     3453. Separate Squares I(javascript)
+// You are given a 2D integer array squares. Each squares[i] = [xi, yi, li] represents the coordinates of the bottom-left point and the side length of a square parallel to the x-axis.
+
+// Find the minimum y-coordinate value of a horizontal line such that the total area of the squares above the line equals the total area of the squares below the line.
+
+// #LeetCode #JavaScript #CodingInterview #Algorithm #DataStructures #Programming #SoftwareEngineering #TechInterview #CodeChallenge #WebDevelopment #100DaysOfCode #Developer #CodingLife
